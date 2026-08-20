add6d4c4f8769ac3322d1e4d79808bb45ef0279b
Chatearn <langatdenniz9@gmail.com>
2026-08-20 09:20:22 +0300

    Set up Express server with dotenv configuration

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "online",
+    message: "ChatPesa payment server is running"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`ChatPesa server running on port ${PORT}`);
+});
